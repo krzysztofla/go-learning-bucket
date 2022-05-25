50c204a7f38ca4abc9eff1eb41a5e6e23e84a3cc
krzysztofla <krzysztof.lach@icloud.com>
2022-05-25 11:05:01 +0200

    Learning pointers in details part 1

--- main.go
+++ main.go
@@ -1,5 +1,22 @@
 package main
 
+import (
+	"fmt"
+	"go-learning-bucket/models"
+)
+
 func main() {
 
+	p := models.NewPerson(" Krzysztof", " Łach", " Kraków", " Sezamkowa  18P")
+
+	personPointer := &p // This means give me memory adress (reference) to object person so I can change something
+
+	(*personPointer).UpdateName("krzysztof") // * pointer - means give me direct access to this object in the memory
+
+	// (*personPointer) this means that pointer is turn into the value witch it this case is person instance
+
+	// this will also work
+	p.UpdateName("Krzysztof 22")
+
+	fmt.Printf("%+v", p)
 }
--- models/person.go
+++ models/person.go
@@ -11,14 +11,26 @@ type Person struct {
 	Contact Contact
 }
 
-func NewPerson(name string, surname string, contact Contact) *Person {
+func NewPerson(name string, surname string, city string, adress string) *Person {
 	return &Person{
 		Name:    name,
 		Surname: surname,
-		Contact: contact,
+		Contact: Contact{
+			City:   city,
+			Adress: adress,
+		},
 	}
 }
 
-func (person *Person) UpdateName(newName string) {
-	(*person).Name = newName
+// the reciver of this function is the pointer that points to instance of a person in the memory
+func (pointToPerson *Person) UpdateName(newName string) {
+	(*pointToPerson).Name = newName
 }
+
+// func (variableName *Person) - this mean that we are working with a pointer to a person. This is also description of a type. In this case is a pointer to person struct type
+// (*pointToPerson) - this mean we want to manipulate the value that pointer is pointing to
+
+// This update function won't work because we don't have access to the memory with the object - we need to use pointers
+// func (person Person) UpdateName(newName string) {
+// 	person.Name = newName
+// }
