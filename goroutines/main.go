45155704a8469b9a52715cac3c813e7397584ba8
krzysztofla <krzysztof.lach@icloud.com>
2022-05-31 12:09:15 +0200

    creating thread blocking code - routines p1

--- goroutines/main.go
+++ goroutines/main.go
@@ -1,5 +1,28 @@
 package main
 
-func main() {
+import (
+	"fmt"
+	"net/http"
+)
 
+func main() {
+	links := make([]string, 0)
+	links = append(links,
+		"http://stackoverflow.com/",
+		"http://www.google.com/",
+		"http://pkg.go.dev/",
+		"http://www.facebook.com/")
+
+	for _, link := range links {
+		checkSiteStatus(link)
+	}
+}
+
+func checkSiteStatus(link string) {
+	_, err := http.Get(link)
+	if err != nil {
+		fmt.Println(link, "webstie is down lol")
+		return
+	}
+	fmt.Println(link, "wow it works")
 }
